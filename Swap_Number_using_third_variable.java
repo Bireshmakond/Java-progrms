cf91483fb2eaf300bfafa3e97ecf69cafb531f35
Biresh <128688883+Bireshmakond@users.noreply.github.com>
2023-03-23 23:24:36 +0545

    Add files via upload

---
+++ Swap_Number_using_third_variable.java
@@ -0,0 +1,32 @@
+package prog;
+
+public class Swap_Number_using_third_variable {
+
+	   public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		System.out.println("before Swapping "+a+" "+b);
+		int temp=a;
+		a=b;
+		b=temp;
+       System.out.println("After swapping "+a+" "+b);
+   }
+   }
+		   
+		   
+		   
+/*Swap_Number_without_ using_third_variable 
+
+     int a=200;
+     int b=100;
+     System.out.println("before Swapping "+a+" "+b);
+     a=a+b;
+     b=a-b;
+     a=a-b;
+     System.out.println("After swapping "+a+" "+b);
+
+    }
+    }*/
+
+
+
